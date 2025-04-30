0d85dcd139655ac52f046ba5e19850003ba2342c
Deepak Kumar <deepakdeepti55@gmail.com>
2025-04-30 19:01:57 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,39 @@
+# print("Hello world")
+# a=10
+# b=9
+# sum=a+b
+# print(sum) 
+# #hi  we are writting a python code
+
+# age=45
+# print(type(age))
+# name=input("name :")
+# print(name)
+# age = int(input("age:"))
+# print(age)
+# price=float(input("price:"))
+# print(price)
+
+# print("My name is ",name,"and i am ",age,"years old")
+
+# a=int(input("enter first num "))
+# b=int(input("enter second num "))
+# c=int(input("enter third num "))
+# if(a>b and a>c):
+#     print(a,"is greater number")
+#  elif(b>a and b>c) 
+#     print(b, "is greater number")
+#  else:
+#     print(c,"is greater number")
+
+
+light = input("light colour :")
+
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("wait")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is broken ")    
